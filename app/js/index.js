318811c1a8dc6f658030613f79b3aa73302276de
liutj@adcc.com.cn <liutj@adcc.com.cn>
2017-12-28 16:28:01 +0800

    Add structor code and demo

---
+++ app/js/index.js
@@ -0,0 +1,7 @@
+//以下引入相关css
+// require('bootstrap/dist/css/bootstrap.min.css')
+require('../css/test.css')
+
+//以下引入相关js
+// require('bootstrap')
+require('./test')
---
+++ app/js/test.js
@@ -0,0 +1,1 @@
+$('.root').html('Hello my Friend~~')
